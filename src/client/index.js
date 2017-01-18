0f80a77fea4f714778a9e9d33c97903287213f57
llanginger <llanginger@gmail.com>
2017-01-17 19:57:15 -0800

    When applied, this commit adds hellow world

---
+++ src/client/index.js
@@ -0,0 +1,9 @@
+const React = require('react')
+const ReactDOM = require('react-dom')
+
+const HelloWorld = (props) => (
+	<div>HI THERE</div>
+)
+
+
+ReactDOM.render(<HelloWorld />, document.getElementById("root"))
